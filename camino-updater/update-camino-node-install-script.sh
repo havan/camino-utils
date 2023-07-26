cdbf4d7c023c44c3b0bf60cf60d2338c63989376
Ekrem Seren <ekrem.seren@gmail.com>
2023-07-27 02:52:11 +0300

    Use camino-node install logic inside the script No need for the auto install script and passwordless sudo anymore

--- camino-updater/update-camino-node-install-script.sh
+++ camino-updater/update-camino-node-install-script.sh
@@ -1,13 +1,58 @@
 #!/bin/bash
 
-# A script to check for latest version of camino-node and install it via install script
-# Prerquisites for this script:
-#  - sudo privilege (passwordless sudo if you want to use it from cron)
+# A script to check for latest version of camino-node and upgrade to that specific version.
+# Keep in mind that this script will downgrade if the latest tagged version lower then the installed one.
+#
+# This script is designed to be run as root from crontab
+#
+# Prerequisites for this script:
 #  - jq command installed
-#  - this script should be in the same directory where the camino-node-installer.sh is located
-#    (script will download installer script if it does exist)
 
+unset -v CAMINO_NODE_USERNAME
+while getopts 'u:h' opt; do
+  case "$opt" in
+    u)
+      CAMINO_NODE_USERNAME="$OPTARG"
+      echo "Starting upgrade process for user: $CAMINO_NODE_USERNAME"
+      ;;
+   
+    ?|h)
+      echo "Usage: $(basename $0) -u username"
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
 
+# Exit if no username is given
+if [ -z "$CAMINO_NODE_USERNAME" ]
+then 
+    echo "Please provide a username with -u option"
+    echo "Usage: $(basename $0) -u username"
+    exit
+fi
+
+if ! id "$CAMINO_NODE_USERNAME" >/dev/null 2>&1; then
+    echo "ERROR: User not found: $CAMINO_NODE_USERNAME"
+    echo "ERROR: Please provide the username that is used to install camino-node."
+    exit
+fi
+
+# Check if we are root or have sudo
+if ! ((EUID == 0)); then
+    echo "ERROR: The script is designed to run as root user. Please run it with sudo prefix."
+    exit
+fi
+
+# Check if camino-node is installed
+if test -f "/etc/systemd/system/camino-node.service"; then
+  echo "Found camino-node systemd service..."
+else
+  echo "ERROR: Can not find camino-node service installed. Please install camino-node to use this upgrade script."
+  exit 255
+fi
+
+# Check if we have jq
 if ! command -v jq &> /dev/null
 then
     echo "jq command could not be found"
@@ -15,9 +60,29 @@ then
     exit 1
 fi
 
-INSTALL_SCRIPT="camino-node-installer.sh"
-INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/chain4travel/camino-docs/c4t/scripts/camino-node-installer.sh"
 LATEST_RELEASE_URL="https://api.github.com/repos/chain4travel/camino-node/releases/latest"
+
+echo "Preparing environment..."
+foundArch="$(uname -m)"                         #get system architecture
+foundOS="$(uname)"                              #get OS
+if [ "$foundOS" != "Linux" ]; then
+  #sorry, don't know you.
+  echo "Unsupported operating system: $foundOS!"
+  echo "Exiting."
+  exit
+fi
+if [ "$foundArch" = "aarch64" ]; then
+  getArch="arm64"                               #we're running on arm arch (probably RasPi)
+  echo "Found arm64 architecture..."
+elif [ "$foundArch" = "x86_64" ]; then
+  getArch="amd64"                               #we're running on intel/amd
+  echo "Found amd64 architecture..."
+else
+  #sorry, don't know you.
+  echo "Unsupported architecture: $foundArch!"
+  echo "Exiting."
+  exit
+fi
 
 # Get latest released version with "latest" tag
 echo -n "Checking latest release..."
@@ -55,6 +120,8 @@ then
 fi
 
 # Do the update if the version does not match the latest version
+# Keep in mind that this will downgrade the camino-node version if the latest 
+# version is lower then the installed one
 if [ "$LATEST_VERSION" != "$CURRENT_VERSION" ]
 then
     # Get the this scripts path
@@ -63,27 +130,44 @@ then
     # Change directory
     cd $SCRIPT_PATH
 
-    # Check if the install script exist and download if not
-    if [[ -f "$INSTALL_SCRIPT" ]]; then
-        echo "Found install script"
-    else
-        echo "Install script does not exist. Downloading latest from $INSTALL_SCRIPT_URL"
-        wget -nd -m $INSTALL_SCRIPT_URL
-        chmod 755 $INSTALL_SCRIPT
-    fi
-    echo "Using install script at: $SCRIPT_PATH/$INSTALL_SCRIPT"
-
-    # Start the update
+    # Start the upgrade
     echo "Current Node version ($CURRENT_VERSION) is NOT the latest ($LATEST_VERSION)"
-    echo "Updating camino-node..."
-    echo "Stopping camino-node service..."
-    sudo systemctl stop camino-node
-    echo "Running auto install script..."
-    INSTALL_COMMAND="$SCRIPT_PATH/camino-node-installer.sh --version $LATEST_VERSION"
-    echo "command: $INSTALL_COMMAND"
-    $INSTALL_COMMAND
+    echo "Upgrading camino-node..."
+    echo "Stopping service..."
+    systemctl stop camino-node
+
+    # Download and copy node files
+    mkdir -p /tmp/camino-node-install               #make a directory to work in
+    rm -rf /tmp/camino-node-install/*               #clean up in case previous install didn't
+    cd /tmp/camino-node-install
+
+    version=$LATEST_VERSION
+
+    fileName="https://github.com/chain4travel/camino-node/releases/download/$version/camino-node-linux-$getArch-$version.tar.gz"
+
+    if [[ `wget -S --spider $fileName  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
+        echo "Node version found."
+    else
+        echo "Unable to find camino-node version $version. Exiting."
+        echo "Restarting service..."
+        systemctl start camino-node
+      exit
+    fi
+
+    echo "Attempting to download: $fileName"
+    wget -nv --show-progress $fileName
+    echo "Unpacking node files..."
+    runuser -l $CAMINO_NODE_USERNAME -c 'mkdir -p $HOME/camino-node'
+    runuser -l $CAMINO_NODE_USERNAME -c 'tar xvf /tmp/camino-node-install/camino-node-linux*.tar.gz -C $HOME/camino-node --strip-components=1;'
+    #rm camino-node-linux-*.tar.gz
+    runuser -l $CAMINO_NODE_USERNAME -c 'echo "Node files unpacked into $HOME/camino-node"'
+
+    echo "Node upgraded, starting service..."
+    systemctl start camino-node
+    echo "New node version:"
+    runuser -l $CAMINO_NODE_USERNAME -c '$HOME/camino-node/camino-node --version'
+    echo "Done!"
+    exit
 else
     echo "Current node version ($CURRENT_VERSION) is the latest ($LATEST_VERSION). Skipping..."
 fi
-echo "FINISHED."
-
